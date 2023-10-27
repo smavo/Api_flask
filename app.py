56f8a0de9afa18740be0d448207753300be5d0a9
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-26 23:41:24 -0500

    Add Post [Store, Item], sin persistencia de Datos

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 stores = [
     {
-        "name": "My Steore",
+        "name": "My Store",
         "items": {
             "name": "Dynamo Soft",
             "price": 18.00
@@ -13,6 +13,28 @@ stores = [
 ]
 
 
-@app.route("/store")
+@app.get("/store")
 def get_store():
     return {"Store": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+
+    return {"message": "Store not Found"}, 404
+
