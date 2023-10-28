91cb43a8ecb68b05d391be0ded1dc611002703a7
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-27 23:29:56 -0500

    Update Rest Api

--- Notas.md
+++ Notas.md
@@ -1,6 +1,17 @@
 
-
+https://flask.palletsprojects.com/en/3.0.x/
+https://pypi.org/project/Flask/
 pip install flask
+
+https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+https://pypi.org/project/Flask-SQLAlchemy/
 pip install flask_sqlalchemy
 
-flask --app app debug run
+flask --app app --debug run
+
+https://flask-smorest.readthedocs.io/en/latest/
+https://pypi.org/project/flask-smorest/
+pip install flask-smorest
+
+https://pypi.org/project/python-dotenv/
+pip install python-dotenv
--- app.py
+++ app.py
@@ -1,55 +1,56 @@
+import uuid
+
 from flask import Flask, request
+from db import stores, items
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": {
-            "name": "Dynamo Soft",
-            "price": 18.00
-        }
-    }
-]
 
-
-@app.get("/store")
+@app.get("/stores")
 def get_stores():
-    return {"Store": stores}
+    return {"Stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")
+@app.post("/item")
 def create_item(name):
-    request_data = request.get_json()
+    item_data = request.get_json()
 
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
+    if item_data["store_id"] not in stores:
+        return {"message": 'Store not found'}, 404
 
-    return {"message": "Store not Found"}, 404
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
 
-
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not Found"}, 404
+    return item, 201
 
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not Found"}, 404
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not Found"}, 404
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not Found"}, 404
+
