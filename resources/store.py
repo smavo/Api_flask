bf30b8603994f8ac656a915e8b4ece4a7aa13746
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-29 09:33:46 -0500

    Add relaciones entre modelos [Store, Item], configuracion de Flask-SQLAlchemy en la aplicacion

--- resources/store.py
+++ resources/store.py
@@ -2,7 +2,7 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+# from db import stores
 from schemas import StoreSchema, StoreUpdateSchema
 
 blp = Blueprint("Stores", "stores", description="Operations on stores")
